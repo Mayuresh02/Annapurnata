39969bc9d6d7a87b02164f2d4af50bc28c056386
Mayuresh Hivarekar <0911.mayuresh@gmail.com>
2024-02-05 19:59:16 +0530

    updated code

--- app/src/main/java/com/mayuresh/annapurnata/Activity/DonationActivity.java
+++ app/src/main/java/com/mayuresh/annapurnata/Activity/DonationActivity.java
@@ -12,5 +12,6 @@ public class DonationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
+
     }
 }
